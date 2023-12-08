93c1d7128e838a14b967265109ecd98fba6f5f4e
Jax Duan <j_duan168326@fanshaweonline.ca>
2023-12-08 03:04:44 -0500

    fixed bugs
    fixed bug

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 (() => {
- 
+ //gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+ //let smoother = ScrollSmoother.create({
+  //wrapper :'.scrollSmooth-con',
+  //contecnt: '.contentSmooth',
+  //smooth: 2,
+  //smoothTouch:0.1
+ //})
+ //const hambuger = document.querySelector("#Hambuger-menu");
+ //const navMenu = document.querySelector(".nav-menu");
+  //hambuger.addEventListener("click", () => {
+  //hambuger.classList.toggle("active");
+  //navMenu.classList.toggle("active");
+  //});
+
+   document.addEventListener("DOMContentLoaded", function () {
+            const hamburger = document.getElementById("Hambuger-menu");
+            const navMenu = document.querySelector(".nav-menu");
+            hamburger.addEventListener("click", function () {
+            hamburger.classList.toggle("active");
+            navMenu.classList.toggle("active");
+            });
+    });
 })();
