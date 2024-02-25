d33567377ba896e98d3adea454dba811b929df3d
Jax Duan <j_duan168326@fanshaweonline.ca>
2024-02-25 16:06:22 -0500

    new
    new

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-(() => {
- function showMenu() {
-    document.querySelector('.navigation').classList.toggle('active');
-    document.querySelector('.hambuger-menu').classList.toggle('hide');
-    document.querySelector('.close').classList.toggle('show');
-}
+ (() => {
+            gsap.registerPlugin(ScrollTrigger);
 
-})();
+            // Add smooth scrolling to all links within the page
+            const links = document.querySelectorAll('a[href^="#"]');
+            links.forEach(link => {
+                link.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    const target = document.querySelector(this.getAttribute('href'));
+                    if (target) {
+                        gsap.to(window, { scrollTo: target, duration: 1, ease: "power2.inOut" });
+                    }
+                });
+            });
+        })();
+
 
 
