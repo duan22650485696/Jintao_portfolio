ed60db747cce16c7043dbd25636394d7a6f4251d
Jax Duan <j_duan168326@fanshaweonline.ca>
2024-02-26 02:14:25 -0500

    add hambuger menu

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
- (() => {
-            gsap.registerPlugin(ScrollTrigger);
-
-            // Add smooth scrolling to all links within the page
-            const links = document.querySelectorAll('a[href^="#"]');
-            links.forEach(link => {
-                link.addEventListener('click', function (e) {
-                    e.preventDefault();
-                    const target = document.querySelector(this.getAttribute('href'));
-                    if (target) {
-                        gsap.to(window, { scrollTo: target, duration: 1, ease: "power2.inOut" });
-                    }
-                });
-            });
-        })();
-
-
+document.getElementById('ham-menu').addEventListener('click', function() {
+    document.querySelector('.nav-menu').classList.toggle('active');
+});
 
