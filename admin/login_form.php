a11a8b3c56588d32cbecad64148c227972939d59
Jax Duan <j_duan168326@fanshaweonline.ca>
2024-03-03 18:36:00 -0500

    add new code
    add new code

--- admin/login_form.php
+++ admin/login_form.php
@@ -3,19 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project Page</title>
+    <title>Login</title>
     <link rel="stylesheet" href="css/main.css" type="text/css">
-
 </head>
 <body>
 
 <form action="login.php" method="post">
-<label for="username">Username: </label>
-<input type="text" name="username" id="username"><br>
-<label for="password">Password: </label>
-<input type="password" name="password" id="password"><br><br>
-<input type="submit" value="login">
+    <label for="username">Username: </label>
+    <input type="text" name="username" id="username"><br>
+    <label for="password">Password: </label>
+    <input type="password" name="password" id="password"><br><br>
+    <input type="submit" value="Login">
 </form>
 
 </body>
-</html>
+</html>>
