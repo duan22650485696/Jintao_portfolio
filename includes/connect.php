5c1c10ac5d0714ae3c71b49156dc9f69551fb5a9
Jax Duan <j_duan168326@fanshaweonline.ca>
2024-03-03 19:03:48 -0500

    upload

--- includes/connect.php
+++ includes/connect.php
@@ -1,13 +1,9 @@
 <?php
-
-//PDO connection
-
 $dsn = "mysql:host=localhost;dbname=portfolio;charset=utf8mb4";
 try {
-$connection = new PDO($dsn, 'root', '');
+$connection = new PDO($dsn, 'root', 'root');
 } catch (Exception $e) {
   error_log($e->getMessage());
   exit('unable to connect');
 }
-
 ?>
